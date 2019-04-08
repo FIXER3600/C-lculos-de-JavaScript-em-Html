e0b8396b836bd96a942b8c2b53205e19820f9167
FIXER3600 <guilhermecomercial51@gmail.com>
2019-04-08 20:54:19 -0300

    Cálculos de JavaScript em Html

---
+++ js/new 1.js
@@ -0,0 +1,8 @@
+function calc(n){
+document.getElementById("resultado").value !=""{
+document.getElementById("resultado")
+			.value= parseFloat(document.getElementById("resultado").value + n; 
+			}
+else{
+document.getElementById("resultado").value= n;
+	}
